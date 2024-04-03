1ddf8d561a1139b96ac022a593c2ae3f50b2758e
Maksim Golubev <maksim.golubev@lightspeedhq.com>
2024-04-03 22:26:12 +0400

    install vue-router and add some pages

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
+    <RouterLink :to="{name: RouteNames.HOME}">Go to home page</RouterLink>
+    <RouterLink :to="{name: RouteNames.AUTH}">Go to home auth</RouterLink>
+  </div>
+  <div>
+    <RouterView />
   </div>
 </template>
 
 <script>
-export default {
+import { RouterView, RouterLink } from "vue-router";
+import { RouteNames } from "@/consts/route-names.js";
 
+export default {
+  components: {
+    RouterView,
+    RouterLink
+  },
+  data: () => ({
+   RouteNames
+  })
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,22 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import {createMemoryHistory, createRouter} from 'vue-router'
+
+import AuthPage from '@/pages/AuthPage.vue'
+import HomePage from '@/pages/HomePage.vue'
+import {RouteNames} from "@/consts/route-names.js";
+
+const routes = [
+    { path: '/', name: RouteNames.HOME, component: HomePage },
+    { path: '/auth', name: RouteNames.AUTH, component: AuthPage },
+]
+
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes,
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app')
